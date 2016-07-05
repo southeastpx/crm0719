0ac599fd40c60f6016cf5d84c3c8cf0150c08693
southeastpx <987758324@qq.com>
2016-07-05 10:45:10 +0800

    response to local
    response to local

--- src/com/sfit/github/test/Test.java
+++ src/com/sfit/github/test/Test.java
@@ -4,5 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("test github!!");
 		System.out.println("hello github!!");
+		System.out.println("github response to local!!");
 	}
 }
