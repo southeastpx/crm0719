1558d29f065035424113ba51005d3fc9ee2d5de3
peng.xing <southeast_px@163.com>
2016-07-05 11:17:44 +0800

    update file 03

--- src/com/sfit/github/test/Test.java
+++ src/com/sfit/github/test/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("test github!!");
 		System.out.println("hello github!!");
+		System.out.println("local response to github server!!");
 		System.out.println("github response to local!!");
 		System.out.println("github response to local 02!!");
 	}
